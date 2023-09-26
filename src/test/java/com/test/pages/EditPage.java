4d58fca0f653aacc9775b74c72293ef0fe41bdae
Burockjohn <brkcn87@gmail.com>
2023-09-26 11:24:45 +0300

    Scenarios created

---
+++ src/test/java/com/test/pages/EditPage.java
@@ -0,0 +1,15 @@
+package com.test.pages;
+
+import com.test.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class EditPage {
+
+    public EditPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+}
