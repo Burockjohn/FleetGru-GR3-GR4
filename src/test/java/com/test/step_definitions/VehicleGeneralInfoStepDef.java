a096017d6f5448beacf2c8dc6db4420b543bdae2
Elif007 <lastnovember@gmail.com>
2023-09-29 11:53:28 +0300

    general ınfo page added

--- src/test/java/com/test/step_definitions/VehicleGeneralInfoStepDef.java
+++ src/test/java/com/test/step_definitions/VehicleGeneralInfoStepDef.java
@@ -1,5 +1,34 @@
 package com.test.step_definitions;
 
-public class VehicleGeneralInfoStepDef {
+import com.test.pages.BasePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class VehicleGeneralInfoStepDef extends BasePage {
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+
+    }
+    @When("User can see the {string}  page by clicking on any vehicle \\(row), under Fleet-Vehicle module")
+    public void user_can_see_the_page_by_clicking_on_any_vehicle_row_under_fleet_vehicle_module(String string) {
+
+    }
+    @When("User can see the {string} page clicking on the Eye \\(View) icon at the end of each row, under Fleet-Vehicle module")
+    public void user_can_see_the_page_clicking_on_the_eye_view_icon_at_the_end_of_each_row_under_fleet_vehicle_module(String string) {
+
+    }
+    @When("Sales manager and store manager should see {string}, {string} and {string} buttons on the {string} page")
+    public void sales_manager_and_store_manager_should_see_and_buttons_on_the_page(String string, String string2, String string3, String string4) {
+
+    }
+    @When("Driver shouldn't see {string}, {string} and {string} buttons")
+    public void driver_shouldn_t_see_and_buttons(String string, String string2, String string3) {
+
+    }
+    @Then("Vehicle information displayed on the {string} page and {string} page should be the same")
+    public void vehicle_information_displayed_on_the_page_and_page_should_be_the_same(String string, String string2) {
+
+    }
 
 }
